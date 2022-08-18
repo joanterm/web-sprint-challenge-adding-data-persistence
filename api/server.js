61754dfce1679f43649578d158bd78f583679f4a
joanterm <jo_joanarg@yahoo.com>
2022-08-18 16:21:18 -0400

    Server up and running

--- api/server.js
+++ api/server.js
@@ -1,1 +1,12 @@
 // build your server here and require it from index.js
+const express = require("express")
+const server = express()
+server.use(express.json())
+
+
+//SANITY CHECK
+server.get("/", (req, res) => {
+    res.send("Express working on sprint challenge!")
+})
+
+module.exports = server
